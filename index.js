7a7cb2ad86c3c8bb2492cf6289e31020d0166897
keithkipkoech <keithselim@gmail.com>
2025-02-28 00:57:50 +0300

    event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//function addingEventListener()) {
+    
+//}
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+
+});
+
 }
+addingEventListener();
